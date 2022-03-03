f3456100e03f48575ec397aa36d8b6a9b725459c
rushitrivedi83 <rushitrivedi83@gmail.com>
2022-03-03 13:36:36 -0500

    Get single Test Backend

--- server/controllers/tests.js
+++ server/controllers/tests.js
@@ -16,6 +16,22 @@ export const getTests = async (req, res) => {
    }
 };
 
+export const getTest = async (req, res) => {
+   try {
+      const { id: testID } = req.params;
+      // Get all of the tests/exams in the DB.
+      const test = await Test.find({_id: testID});
+
+      console.log("Sending test with id: ", testID , ":", {test});
+
+      // Send an array of all the tests.
+      res.status(200).json(test);
+   } catch (error) {
+      res.status(404).json({ message: error.message });
+   }
+};
+
+
 
 export const getStudentTests = async (req, res) => {
    try {
--- server/routes/tests.js
+++ server/routes/tests.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getTests, createTest, updateTest, getStudentTests} from '../controllers/tests.js'
+import { getTests, createTest, updateTest, getStudentTests, getTest} from '../controllers/tests.js'
 
 const router = express.Router();
 
@@ -9,5 +9,5 @@ router.get('/', getTests); // Get all the tests in the DB.
 router.post('/', createTest); // Create a new test.
 router.patch('/:id', updateTest); // Update an existing Post. (We need an test id before we can update a test.)
 router.get('/students', getStudentTests);
-
+router.get('/:id', getTest);
 export default router;
