1e666abda359c5cef4d4f3217e0baee866670ec4
markkcruz <markkcruz@optonline.net>
2022-03-07 20:33:25 -0500

    Backend getAnswers(testID) route fix

--- server/routes/answers.js
+++ server/routes/answers.js
@@ -5,7 +5,7 @@ import { getStudentAnswers,  createAnswer, getAnswer } from '../controllers/answ
 const router = express.Router();
 
 // localhost:5000/api/answers
-router.get('/:testid', getStudentAnswers); // Get all the Student Test Answers from the DB based on the testID
+router.get('/:id', getStudentAnswers); // Get all the Student Test Answers from the DB based on the testID
 router.post('/', createAnswer); // create new Student Test Answer the DB.
 router.get('/answer/:id', getAnswer); 
 
