86898b797519ec57ce86713afe876d7c2d965787
rushitrivedi83 <rushitrivedi83@gmail.com>
2022-03-08 00:41:42 -0500

    Update Scores Functionality

--- server/routes/scores.js
+++ server/routes/scores.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getStudentScores } from '../controllers/scores.js'
+import { getStudentScores, updateScore } from '../controllers/scores.js'
 
 const router = express.Router();
 
 // localhost:5000/posts
-router.get('/:username/:testid', getStudentScores); // Get all the tests in the DB.
+router.get('/:username/:testid', getStudentScores); // Get all the Scores in the DB.
+router.post('/:username/:testid', updateScore); // Get all the Scores in the DB.
 
 export default router;
